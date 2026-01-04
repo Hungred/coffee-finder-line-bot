4c9437e3421ace26fc15abceff496a96bf0057e5
Hungred <anita397892@gmail.com>
2026-01-04 23:23:07 +0800

    feat: 新增搜尋咖啡廳api

---
+++ src/api/cafes.js
@@ -0,0 +1,18 @@
+const isDev = process.env.NODE_ENV !== 'production';
+
+const API_BASE = isDev
+  ? 'http://localhost:3000/api/cafes'
+  : process.env.API_BASE_URL;
+
+export async function fetchCafes({ searchQuery }) {
+  const url = new URL(API_BASE);
+
+  if (searchQuery) {
+    url.searchParams.set('searchQuery', searchQuery);
+  }
+
+  console.log('實際打 API URL:', url.toString());
+
+  const res = await fetch(url.toString());
+  return res.json();
+}
