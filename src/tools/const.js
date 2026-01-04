2644cc361436763bea341ddbb930e7749349906f
Hungred <anita397892@gmail.com>
2026-01-04 23:21:56 +0800

    feat: 新增const

---
+++ src/tools/const.js
@@ -0,0 +1,23 @@
+const CITIES = {
+  changhua: '彰化',
+  chiayi: '嘉義',
+  hsinchu: '新竹',
+  hualien: '花蓮',
+  kaohsiung: '高雄',
+  keelung: '基隆',
+  kinmen: '金門',
+  lienchiang: '連江',
+  miaoli: '苗栗',
+  nantou: '南投',
+  penghu: '澎湖',
+  pingtung: '屏東',
+  taichung: '台中',
+  tainan: '台南',
+  taipei: '台北',
+  taitung: '台東',
+  taoyuan: '桃園',
+  yilan: '宜蘭',
+  yunlin: '雲林',
+};
+
+export { CITIES };
